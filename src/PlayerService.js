8e271a2b5d5ece0924ce4bc4e72ed319e688bf88
TV <ab@cde.com>
2017-11-27 23:53:38 +0200

    fetch added

--- src/PlayerService.js
+++ src/PlayerService.js
@@ -6,9 +6,33 @@ class PlayerService{
   //   this.getPlayers = this.getPlayers.bind(this);
   // }
 
+  savePlayer(player) {
+    console.log('PlayerService savePlayer called');
+    fetch('http://localhost:8080/savePlayer ', {  // http://fribastar.ddns.net:8080/fribaStar/addPlayer
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: player
+      })
+    })
+  }
+
 
   getPlayers() {
     console.log('PlayerService getPlayers called');
+    fetch('http://localhost:8080/test')  // http://fribastar.ddns.net:8080/fribaStar/test getPlayers
+    .then(function(response) {
+      return response.text()
+    }).then(function(json) {
+      console.log('parsed json', json)
+      // return json
+    }).catch(function(ex) {
+      console.log('parsing failed', ex)
+      // return null
+    })
+
     return ['Vesa', 'Samu'];
   }
 
